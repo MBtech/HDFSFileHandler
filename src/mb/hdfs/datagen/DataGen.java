940e9cc134bbfcfce4852f82274fadf0de8a105c
MBtech <mbilal_ce@live.com>
2014-10-02 03:22:46 +0200

    First Commit

---
+++ src/mb/hdfs/datagen/DataGen.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mb.hdfs.datagen;
+
+/**
+ *
+ * @author mb
+ */
+import org.apache.commons.lang3.RandomUtils;
+import java.util.Random;
+public class DataGen {
+    public byte[] randDataGen(){
+        byte[] randbuf = RandomUtils.nextBytes(new Random().nextInt(100));
+        return randbuf;
+    }
+}
